d022e205b1d13da1fe3fe7539b92c42382e481ab
Sorrow17 <sorrow17@hotmail.com>
2014-08-02 01:06:39 +0800

    Hide src of sorrow17 temporarily

--- sorrow17-src/sample/resources/js/test.js
+++
@@ -1,2 +0,0 @@
-var rocker = require('./avrg.js');
-console.log(rocker("1 2 3 4"));
--- sorrow17-src/sample/src/cljs/avrg.cljs
+++
@@ -1,8 +0,0 @@
-(ns sample
-	(:require [clojure.string :as string]))
-(defn avrg [msg] 
-	(let [x (get (.-match msg) 1) nums (map js/parseFloat (string/split x #"\s+"))] 
-		(.send msg (/ (apply + nums) (count nums))) ))
-
-(set! *main-cli-fn* (fn [& args] nil))
-(aset js/module "exports" (fn [robot] (.respond robot #"(?i)avrg\s+(.*)" avrg)))
